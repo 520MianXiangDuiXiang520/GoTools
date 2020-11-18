89205364c4901d015e796e11a273aae0af7de1d7
520MianXiangDuiXiang520 <15364968962@163.com>
2020-11-18 10:41:15 +0800

    添加几个数据库和配置文件的小工具

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/520MianXiangDuiXiang520/GinTools
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+)
---
+++ utils/setting.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+	"path"
+	"runtime"
+)
+
+type DBSetting struct {
+	Engine   string `json:"engine"`
+	DBName   string `json:"db_name"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+}
+
+type Setting struct {
+	Database *DBSetting `json:"database"`
+}
+
+func (setting *Setting) load(path string) {
+	fp, err := os.Open(path)
+	if err != nil {
+		ExceptionLog(err, "Fail to open setting")
+		panic(err)
+	}
+	defer fp.Close()
+	decoder := json.NewDecoder(fp)
+	err = decoder.Decode(&setting)
+	if err != nil {
+		ExceptionLog(err, "Fail to decode json setting")
+		panic(err)
+	}
+}
+
+var setting *Setting
+
+func InitSetting(fPath string) {
+	_, currently, _, _ := runtime.Caller(1)
+	filename := path.Join(path.Dir(currently), fPath)
+	s := Setting{}
+	s.load(filename)
+	setting = &s
+}
+
+func GetSetting() Setting {
+	if setting == nil {
+		panic("Configuration file not loaded")
+	}
+	return *setting
+}
