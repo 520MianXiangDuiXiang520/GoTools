8fd817e46499dbb7c862f04f219d6b00c5366e54
520MianXiangDuiXiang520 <15364968962@163.com>
2020-12-28 15:33:03 +0800

    :bug: 修复绝对路径拼接错误 :pencil: 完善日志

--- json/json.go
+++ json/json.go
@@ -2,7 +2,10 @@ package json
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/520MianXiangDuiXiang520/GinTools/log_tools"
+	"github.com/520MianXiangDuiXiang520/GinTools/path_tools"
 	"os"
 	"path"
 	"reflect"
@@ -10,19 +13,18 @@ import (
 )
 
 func load(obj interface{}, path string) {
-	if reflect.ValueOf(obj).Elem().Kind() != reflect.Struct {
-		panic("obj is not a struct")
-	}
 	fp, err := os.Open(path)
 	if err != nil {
-		utils.ExceptionLog(err, "Fail to open obj")
+		msg := fmt.Sprintf("Fail to open %s", path)
+		utils.ExceptionLog(err, msg)
 		panic(err)
 	}
 	defer fp.Close()
 	decoder := json.NewDecoder(fp)
 	err = decoder.Decode(&obj)
 	if err != nil {
-		utils.ExceptionLog(err, "Fail to decode json obj")
+		msg := fmt.Sprintf("Fail to decode data to %v", obj)
+		utils.ExceptionLog(err, msg)
 		panic(err)
 	}
 }
@@ -30,9 +32,13 @@ func load(obj interface{}, path string) {
 // FromFileLoadToObj 从 fPath 路径下的 json 文件中加载数据到 s 对象
 func FromFileLoadToObj(s interface{}, fPath string) {
 	if reflect.ValueOf(s).Kind() != reflect.Ptr {
+		msg := "The s must be a ptr type"
+		utils.ExceptionLog(errors.New("TypeError"), msg)
 		panic("The s is not a ptr")
 	}
-	_, currently, _, _ := runtime.Caller(1)
-	filename := path.Join(path.Dir(currently), fPath)
-	load(s, filename)
+	if !path_tools.IsAbs(fPath) {
+		_, currently, _, _ := runtime.Caller(1)
+		fPath = path.Join(path.Dir(currently), fPath)
+	}
+	load(s, fPath)
 }
